830ddad2114cf287eb5c2ed8a17f8aa43db684f9
joviicamarrgo <161964166+joviicamarrgo@users.noreply.github.com>
2024-07-22 13:33:34 +0200

    Update app.js

--- pro/app.js
+++ pro/app.js
@@ -1,7 +1,25 @@
 var pass = document.getElementById("password");
 var msg = document.getElementById("message");
 var str = document.getElementById("strenght");
-
+<html lang="en" dir="rtl">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <title>يوم التأسيس</title>
+  <link rel="icon" href="imgs/icon.png">
+</head><html lang="en" dir="rtl">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <title>يوم التأسيس</title>
+  <link rel="icon" href="imgs/icon.png">
+</head>
 pass.addEventListener('input', () => {
     if (pass.value.length > 0) {
         msg.style.display = "block";
